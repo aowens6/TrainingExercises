bae0362db8c17acdd96141609593c25a7e424725
Austyn <austyn.owens1@gmail.com>
2022-03-21 13:42:01 -0500

    initial commit -- first todo list

---
+++ src/ToDo.java
@@ -0,0 +1,17 @@
+public class ToDo {
+
+  public static void main(String[] args) {
+    System.out.println("Your Dev-Attendable ToDo List");
+    System.out.println("******************************");
+    System.out.println("Due:");
+    System.out.println("Bake Bread.");
+    System.out.println("Walk The dog.");
+    System.out.println("Take over the world");
+    System.out.println("");
+    System.out.println("Completed:");
+    System.out.println("Workout.");
+    System.out.println("Feed cat");
+    System.out.println("Set the world free");
+  }
+
+}
